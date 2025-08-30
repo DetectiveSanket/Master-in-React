53ce90525d06803941472cf9dbc30e8f5df895e9
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-08-31 00:36:57 +0530

    feat: enhance useState example with improved documentation and formatting

--- State Management/src/State Management/useState_Example.jsx
+++ State Management/src/State Management/useState_Example.jsx
@@ -4,14 +4,14 @@ import React, { useState } from 'react'
 //? ⁢initializer function Example
 
 function createInitialTodos() {
-  const initialTodos = [];
-  for (let i = 0; i < 50; i++) {
-    initialTodos.push({
-      id: i,
-      text: 'Item ' + (i + 1)
-    });
-  }
-  return initialTodos;
+    const initialTodos = [];
+    for (let i = 0; i < 50; i++) {
+            initialTodos.push({
+            id: i,
+            text: 'Item ' + (i + 1)
+        });
+    }
+    return initialTodos;
 }
 
 function useState_Example() {
@@ -70,6 +70,7 @@ export default useState_Example;
 
 /* 
     # Details Information
+    ? https://react.dev/reference/react/useState
 
     1. State Management in React (Data Management)
 
@@ -85,8 +86,33 @@ export default useState_Example;
         - It allows you to add state to your components without converting them to class components.
         - The useState hook returns an array with two elements: the current (initial value) state value and a function to update it.
 
-        Parameters 
+       * Parameters 
             • ⁡⁣⁢⁣initialState⁡: The value you want the state to be initially. It can be a value of any type, but there is a special behavior for functions. This argument is ignored after the initial render.
 
             • If you pass a function as initialState, it will be treated as an ⁡⁣⁣⁢initializer function.⁡ It should be pure, should take no arguments, and should return a value of any type. React will call your initializer function when initializing the component, and store its return value as the initial state.
+
+       * Returns 
+            • useState returns an array with exactly two values:
+
+        - The current state. During the first render, it will match the initialState you have passed.
+        - The set function that lets you update the state to a different value and trigger a re-render.
+
+       * Caveats 
+        - useState is a Hook, so you can only c⁡⁣⁣⁢all it at the top level⁡ of your component or your own Hooks. You ⁡⁣⁣⁢can’t call it inside loops or conditions⁡. If you need that, extract a new component and move the state into it. 
+        
+        • ⁡⁣⁢⁣set functions⁡, like setSomething(nextState) 
+             - The set function returned by useState lets you update the state to a different value and trigger a re-render. 
+             - You can pass the next state directly, or a function that calculates it from the previous state:
+             ex:- const [count, setCount] = useState(0); -> here set function is : setCount
+        
+        * Parameters 
+            • ⁡⁣⁢⁣nextState⁡: The value that you want the state to be. It can be a value of any type, but there is a special behavior for functions.
+
+            • If you pass a function as nextState, it will be treated as an updater function. It must be pure, should take the pending state as its only argument, and should return the next state. React will put your updater function in a queue and re-render your component. During the next render, React will calculate the next state by applying all of the queued updaters to the previous state. See an example below.
+
+        * Returns
+            - set functions do not have a return value.
+
+        * Caveats
+            - The set function only updates the state variable for the next render. If you read the state variable after calling the set function, you will still get the old value that was on the screen before your call.
 */
