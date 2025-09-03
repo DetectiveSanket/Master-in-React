17c1d028f83a518d818402cacdddf274291bb490
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-03 12:39:33 +0530

    feat: enhance documentation in UseContextHooks component to clarify context usage and address prop drilling

--- 5_React Hooks/src/Hooks/UseContext_Hooks.jsx
+++ 5_React Hooks/src/Hooks/UseContext_Hooks.jsx
@@ -37,8 +37,13 @@ export { UserContext };
 
 /* 
     # 𝘂𝘀𝗲𝗖𝗼𝗻𝘁𝗲𝘅𝘁 𝗛𝗼𝗼𝗸
-        • useContext is a hook that lets you subscribe to React context without introducing nesting.
-        • It simplifies the process of consuming context values in functional components.
+        - React Context is a way to manage state globally.
+        - It can be used together with the useState Hook to share state between deeply nested components more easily than with useState alone.
+
+        • The Problem
+            - State should be held by the highest parent component in the stack that requires access to the state.
+            - To illustrate, we have many nested components. The component at the top and bottom of the stack need access to the state.
+            - To do this without Context, we will need to pass the state as "props" through each nested component. This is called "prop drilling".
 
         • Basic syntax:
             const value = useContext(MyContext);
