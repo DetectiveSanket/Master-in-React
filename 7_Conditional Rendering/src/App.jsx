3a66e19e46f02e5026d1f49a7e3ae43cb0ee91e4
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-07 23:15:34 +0530

    feat: implement conditional rendering with Login and Logout buttons

--- 7_Conditional Rendering/src/App.jsx
+++ 7_Conditional Rendering/src/App.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react';
 import './App.css'
+import LogoutBtn from './components/LogoutBtn';
+import LoginBtn from './components/LoginBtn';
 
 function App() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+    //*  1> if/else statement 
+            
+        // if(isLoggedIn) {
+        //     return (
+        //         <LogoutBtn />
+        //     )
+        // }
+        // else {
+        //     return (
+        //         <LoginBtn />
+        //     )
+        // }
+        //> isLoggedIn decides which JSX block will be returned.
+
+    //* 4) Early Returns
+        if(!isLoggedIn) {
+            return <LoginBtn />
+        }
+    
+        
+        
 
     return (
         <>
@@ -13,6 +40,26 @@ function App() {
                 | ├── Logical && Operator   
                 | └── Switch Statements    
             </p>
+
+            {
+                //* 2> Ternary Operator
+                isLoggedIn ? <LogoutBtn /> : <LoginBtn />
+            }
+
+            {
+                //* 3> Logical && Operator
+                isLoggedIn && <LogoutBtn />
+            }
+
+            {//* 4> Early Returns
+                // if(!isLoggedIn) {
+                //     return <LoginBtn />
+                // }
+                // return <LogoutBtn />
+            }
+
+            
+           
         </>
     )
 }
@@ -22,4 +69,25 @@ export default App;
 
 /* 
     ⁡⁢⁣⁣# 𝗖𝗼𝗻𝗱𝗶𝘁𝗶𝗼𝗻𝗮𝗹 𝗥𝗲𝗻𝗱𝗲𝗿𝗶𝗻𝗴 𝗶𝗻 𝗥𝗲𝗮𝗰𝘁⁡
-*/
+        - Conditional rendering in React allows you to render different components or elements based on certain conditions. This is typically done using JavaScript conditional statements like if/else, ternary operators, logical && operators, or switch statements.
+        - It helps in creating dynamic and interactive user interfaces by displaying content based on user actions, application state, or other factors.
+
+       ⁡⁢⁣⁣• ⁡⁣⁢⁣𝗜𝗳/𝗘𝗹𝘀𝗲 𝗦𝘁𝗮𝘁𝗲𝗺𝗲𝗻𝘁⁡⁡
+            - The simplest way to render conditionally is using a normal if/else statement in JavaScript.
+            - You can use if/else statements to decide which component or element to render based on a condition.
+            - You decide what JSX to return before rendering.
+
+
+       ⁡⁢⁣⁣• ⁡⁣⁢⁣⁡⁣⁢⁣⁡⁣⁢⁣𝗧𝗲𝗿𝗻𝗮𝗿𝘆 𝗢𝗽𝗲𝗿𝗮𝘁𝗼𝗿⁡⁡
+            - The ternary operator is a concise way to perform conditional rendering in React.
+            - It takes three operands: a condition, an expression to execute if the condition is true, and an expression to execute if the condition is false.
+            - The syntax is: condition ? expressionIfTrue : expressionIfFalse.
+            - It is often used for simple conditional rendering in JSX.
+
+       ⁡⁢⁣⁣•⁡ ⁡⁣⁢⁣𝗟𝗼𝗴𝗶𝗰𝗮𝗹 && 𝗢𝗽𝗲𝗿𝗮𝘁𝗼𝗿⁡
+            - condition && expression
+            - If condition is true → renders the expression.
+            - If condition is false → renders nothing.
+
+       • ⁡⁣⁢⁣𝗦𝘄𝗶𝘁𝗰𝗵 𝗦𝘁𝗮𝘁𝗲𝗺𝗲𝗻𝘁⁡
+*/  
---
+++ 7_Conditional Rendering/src/components/LoginBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoginBtn() {
+    return (
+        <div>
+            
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default LoginBtn
