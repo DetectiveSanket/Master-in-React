0b6defc4be8d760e973c00cce3900092c1cce50e
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-11 00:03:27 +0530

    feat: initialize React project with Vite and ESLint configuration
    - Added ESLint configuration for JavaScript and React with recommended settings.
    - Created index.html as the entry point for the application.
    - Set up package.json with necessary dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application layout and components.
    - Implemented a basic App component in App.jsx.
    - Added React logo SVG in the assets directory.
    - Defined global styles in index.css for consistent theming and layout.
    - Set up main.jsx to render the App component using React's StrictMode.
    - Configured Vite with React plugin for optimal development experience.

---
+++ 13_State Management Libraries/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+
+
+    return (
+      <>
+        <h1>State Management Libraries</h1>
+        <p>
+            |__13.
+            |── State Management Libraries
+            | ├── Redux                 
+            | | ├── Actions            
+            | | ├── Reducers           
+            | | ├── Store              
+            | | ├── connect()          
+            | | └── useSelector/useDispatch 
+            | ├── Redux Toolkit         
+            | ├── Zustand              
+            | ├── Recoil               
+            | └── Context + useReducer
+        </p>
+      </>
+    )
+}
+
+export default App;
+
+/* 
+    # ⁡⁢⁣⁣𝗦𝘁𝗮𝘁𝗲 𝗠𝗮𝗻𝗮𝗴𝗲𝗺𝗲𝗻𝘁 𝗟𝗶𝗯𝗿𝗮𝗿𝗶𝗲𝘀⁡
+*/
