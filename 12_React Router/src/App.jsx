3c1cc33196c8d342aebbffa48b17b934217316c5
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-13 12:02:14 +0530

    feat: initialize React Router project with Vite setup
    - Added package.json with dependencies for React, ReactDOM, and React Router.
    - Included Vite configuration for development and build processes.
    - Created main application structure with App component and basic routing setup.
    - Added CSS styles for layout and design.
    - Included SVG assets for React and Vite logos.
    - Set up index file for rendering the application.

---
+++ 12_React Router/src/App.jsx
@@ -0,0 +1,67 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+        <>
+            <h1>React Router Dom</h1>
+            <p>
+                |__12.
+                |── React Router
+                | ├── Installation & Setup  
+                | ├── Basic Routing         
+                | ├── Route Parameters      
+                | ├── Nested Routes         
+                | ├── Navigation            
+                | | ├── Link Component     
+                | | ├── NavLink Component  
+                | | └── useNavigate Hook   
+                | ├── Route Guards          
+                | └── Dynamic Routing 
+            </p>
+        </>
+    )
+}
+
+export default App;
+
+/* 
+    ⁡⁢⁣⁣## 𝗥𝗲𝗮𝗰𝘁 𝗥𝗼𝘂𝘁𝗲𝗿 𝗗𝗼𝗺⁡
+
+        - React Router Dom is a powerful library for handling routing in React applications. It allows developers to create single-page applications with dynamic routing capabilities, enabling seamless navigation between different views or components without requiring a full page reload.
+
+        * React Router is a library (not built into React) that lets you handle routing (switching pages/views) in a React app.
+        - Without it, React apps are just single-page — you’d need to manually show/hide components.
+        - React Router gives your app URLs, navigation, and browser history handling like a normal website.
+
+        Q) why do we need React Router?
+        A) In a React application, we often want to create multiple views or pages that users can navigate between. React Router provides a way to define these routes and manage the navigation between them seamlessly.
+
+        ⁡⁣⁢⁣• Installation & SetUp⁡
+        ⁡⁣⁢⁣• Basic Routing⁡
+        ⁡⁣⁢⁣• Route Parameters⁡
+
+*/
+
+// 1. npm install react-router-dom
+// 2. BrowserRouter as Router
+// 3. Routes
+// 4. Route
+// 5. Link
+// 6. NavLink
+// 7. useParams
+// 8. useNavigate
+// 9. Navigate
+// 10. Outlet
+// 11. Custom 404 Page
+// 12. Nested Routing
+// 13. Index Routes
+// 14. URL Search Params
+// 15. Programmatic Navigation
+// 16. Protected Routes (Auth)
+// 17. Lazy Loading Routes (Code Splitting)
+// 18. Route Transition Animations
+// 19. Scroll Restoration
+// 20. Query Parameters and State Management
