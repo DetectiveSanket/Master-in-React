7f0c178376075e4c33ca59d63e9463e9a788218a
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-14 12:30:05 +0530

    feat: initialize Error Boundaries project with Vite and React
    - Added ESLint configuration for code linting.
    - Created index.html for the application entry point.
    - Set up package.json with dependencies and scripts for development, build, and linting.
    - Included Vite logo SVG in the public directory.
    - Added App.css for styling the application.
    - Implemented App.jsx as the main application component.
    - Included React logo SVG in the assets directory.
    - Created index.css for global styles.
    - Set up main.jsx for rendering the application using React's StrictMode.
    - Configured Vite for the project.

---
+++ 17_Error Boundaries/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+        <>
+            <h1>Error Boundaries</h1>
+            <hr />
+            <p>
+            |__17.
+            |── Error Boundaries
+            | ├── Error Boundary Class  
+            | ├── componentDidCatch     
+            | ├── getDerivedStateFromError 
+            | └── Error Boundary Libraries 
+            </p>
+        </>
+    )
+}
+
+export default App;
+
+/* 
+    ⁡⁢⁣⁣## 𝗘𝗿𝗿𝗼𝗿 𝗕𝗼𝘂𝗻𝗱𝗮𝗿𝗶𝗲𝘀⁡
+*/
